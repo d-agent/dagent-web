3f9f456c4670b475945bd42ee4704c2a421d7e80
Adhar Battulwar <adharbattulwar24@gmail.com>
2025-09-27 11:08:15 +0000

    added the create delete and fetch of apis

--- src/hooks/apikey.ts
+++ src/hooks/apikey.ts
@@ -1,24 +1,60 @@
 import ApiKeyService from "@/lib/api/apikey";
 import { authClient } from "@/lib/auth-client";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
+export const useGetApiKeyList = () => {
+  return useQuery({
+    queryKey: ["apiKeys"],
+    queryFn: async () => {
+      const { data, error } = await authClient.apiKey.list();
+      if (error) throw error;
+      return data;
+    },
+  });
+};
 
-export const useGetApiKeyList = async ()  => { 
-    const {data, error} = await authClient.apiKey.list();
-    return {data, error};
-}
+export const useDeleteApiKey = () => {
+  const queryClient = useQueryClient();
 
-export const useDeleteApiKey = async (key_id: string) => {
-    const {data, error} = await authClient.apiKey.delete({keyId: key_id});
-    return {data, error};
-}
+  return useMutation({
+    mutationFn: async (key_id: string) => {
+      const { data, error } = await authClient.apiKey.delete({ keyId: key_id });
+      if (error) throw error;
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["apiKeys"] });
+    },
+  });
+};
 
-export const useGetApiKey = async (id: string) => {
-    const { data, error } = await authClient.apiKey.get({ query: { id: id } });
+export const useGetApiKey = (id: string) => {
+  return useQuery({
+    queryKey: ["apiKey", id],
+    queryFn: async () => {
+      const { data, error } = await authClient.apiKey.get({ query: { id } });
+      if (error) throw error;
+      return data;
+    },
+    enabled: !!id, // prevents running when id is undefined
+  });
+};
 
-    return {data, error}
-}
+export const useCreateApiKey = () => {
+  const queryClient = useQueryClient();
 
-export const useCreateApiKey = async (name: string) => {
-    const data = await ApiKeyService.createApiKey({name});
-    return data;
-}
+  return useMutation({
+    mutationFn: async (name: string) => {
+      const data = await ApiKeyService.createApiKey({ name });
+    //   if (error) throw error;
+      return data;
+    },
+    onSuccess: (data) => {
+      console.log("API Key created successfully:", data);
+      queryClient.invalidateQueries({ queryKey: ["apiKeys"] });
+    },
+    onError: (error) => {
+      console.error("Error creating API Key:", error);
+    },
+  });
+};
